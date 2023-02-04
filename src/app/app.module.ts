86640d094b6f8ba9baf051b873fed20bcc46fcea
marco-schultz-unvoid <marco.schultz@unvoid.io>
2023-02-04 12:14:42 -0300

    Main page component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MainPageModule } from './pages/main-page/main-page.module';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, MainPageModule],
   providers: [],
   bootstrap: [AppComponent],
 })
