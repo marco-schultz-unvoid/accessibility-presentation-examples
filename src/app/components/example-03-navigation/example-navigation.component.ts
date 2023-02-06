e0faf9ed7fc73cae6b381487f8ed9beef5e3fdbd
marco-schultz-unvoid <marco.schultz@unvoid.io>
2023-02-06 09:03:59 -0300

    Header will be presented by unvoid www

--- src/app/components/example-03-navigation/example-navigation.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'accessibily-presentation-example-navigation',
-  templateUrl: './example-navigation.component.html',
-  styleUrls: ['./example-navigation.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ExampleNavigationComponent {}
