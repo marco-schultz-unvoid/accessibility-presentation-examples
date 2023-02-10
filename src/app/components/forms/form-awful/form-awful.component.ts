3296eef417964623c2e0dc014571f55bf35c2bf2
marco-schultz-unvoid <marco.schultz@unvoid.io>
2023-02-10 11:18:13 -0300

    Awful accessibility form

--- src/app/components/forms/form-awful/form-awful.component.ts
+++ src/app/components/forms/form-awful/form-awful.component.ts
@@ -1,4 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import {
+  getInputValidationMessage,
+  markControlsAsFilled,
+  shouldShowErrorMessage,
+} from 'src/app/utils/functions/forms';
+import { requiredAfterTrimValidator } from 'src/app/utils/validators/requiredAfterTrim';
+import { urlValidator } from 'src/app/utils/validators/url';
 
 @Component({
   selector: 'accessibily-presentation-form-awful',
@@ -6,4 +14,41 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./form-awful.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormAwfulComponent {}
+export class FormAwfulComponent {
+  /* Form */
+  public form = this._fb.group({
+    name: this._fb.control('', [
+      Validators.required,
+      Validators.maxLength(50),
+      requiredAfterTrimValidator,
+    ]),
+    email: this._fb.control('', [
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(50),
+      requiredAfterTrimValidator,
+    ]),
+    website: this._fb.control('', [urlValidator, Validators.maxLength(50)]),
+  });
+
+  /* Helpers */
+  public getErrorMessage = getInputValidationMessage;
+  public shouldShowError = shouldShowErrorMessage;
+
+  /* Lifecycle methods & constructor */
+  constructor(private readonly _fb: NonNullableFormBuilder) {}
+
+  /* Submit */
+  submitHandler(): void {
+    const { name, email, website } = this.form.controls;
+
+    if (this.form.valid) {
+      alert(
+        `Name: ${name.value},\nEmail: ${email.value},\nWebsite: ${website.value}`
+      );
+      return void 0;
+    }
+
+    markControlsAsFilled(this.form.controls);
+  }
+}
