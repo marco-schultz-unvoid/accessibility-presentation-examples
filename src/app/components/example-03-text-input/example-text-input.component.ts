a7ef7f65ec418dceb58e34ab1c50534fd4a85008
marco-schultz-unvoid <marco.schultz@unvoid.io>
2023-02-10 10:15:44 -0300

    Example text input controls implemented

--- src/app/components/example-03-text-input/example-text-input.component.ts
+++ src/app/components/example-03-text-input/example-text-input.component.ts
@@ -1,9 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+type FormType = 'awful' | 'bad' | 'good';
+
 @Component({
   selector: 'accessibily-presentation-example-text-input',
   templateUrl: './example-text-input.component.html',
   styleUrls: ['./example-text-input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExampleTextInputComponent {}
+export class ExampleTextInputComponent {
+  openedForm?: FormType;
+
+  openForm = (form: FormType): void => {
+    this.openedForm = form;
+  };
+}
